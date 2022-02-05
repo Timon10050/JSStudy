4ff5346c4a4ee919dcc5e01c6db616cdbba356d9
Artem Belyaev <arthouse98@gmail.com>
2022-02-05 16:32:56 +0300

    добавлен комит  2а файла

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = 0,
+      screens = 0,
+      screenPrice = 0,
+      rollback = 0,
+      fullPrice = 0,
+      adaptive = 0;
+
+
+alert("Начало положено!");
+
+console.log ("hello js!");
